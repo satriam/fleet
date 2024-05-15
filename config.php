67b5459951f752d962eb96da336bec35398e215b
satria <satria@satrias-MacBook-Air.local>
2024-05-15 09:51:24 +0700

    footer

--- config.php
+++ config.php
@@ -9,6 +9,7 @@ $conn = mysqli_connect($host, $username, $password, $dbname);
 if (!$conn){
         die("Connection Failed:".mysqli_connect_error());
     }
+    var_dump($conn);
     
 date_default_timezone_set('Asia/Jakarta');   
 ?>
